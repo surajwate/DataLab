463dcbb18c785101d9d4c0b4cb8ecf2f420c39fa
Suraj Wate <suraj.wate@gmail.com>
2024-08-28 15:43:38 +0100

    improved tests

--- tests/test_clean.py
+++ tests/test_clean.py
@@ -1,13 +1,14 @@
 import pytest
 import pandas as pd
+import numpy as np  # Import numpy
 from suraj_datalab.clean import RareCategoryReplacer
 
 def test_rare_category_replacer_fit():
-    # Create a test DataFrame
+    # Create a test DataFrame with some rare categories
     data = {
-        'Column1': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'],
-        'Column2': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'],
-        'Column3': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+        'Column1': ['A', 'A', 'A', 'A', 'A', 'B', 'C', 'D', 'E', 'F'],
+        'Column2': ['A', 'A', 'A', 'A', 'A', 'B', 'C', 'D', 'E', 'F'],
+        'Column3': ['A', 'A', 'A', 'A', 'A', 'B', 'C', 'D', 'E', 'F']
     }
     df = pd.DataFrame(data)
 
@@ -18,14 +19,20 @@ def test_rare_category_replacer_fit():
     replacer.fit(df)
 
     # Check that the rare_categories_ attribute is correctly populated
-    assert replacer.rare_categories_ == {'Column1': [], 'Column2': [], 'Column3': []}
+    expected_rare_categories = {
+        'Column1': ['B', 'C', 'D', 'E', 'F'],
+        'Column2': ['B', 'C', 'D', 'E', 'F'],
+        'Column3': ['B', 'C', 'D', 'E', 'F']
+    }
+    for column in expected_rare_categories:
+        assert np.array_equal(replacer.rare_categories_[column], expected_rare_categories[column])
 
 def test_rare_category_replacer_transform():
-    # Create a test DataFrame
+    # Create a test DataFrame with some rare categories
     data = {
-        'Column1': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'],
-        'Column2': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'],
-        'Column3': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+        'Column1': ['A', 'A', 'A', 'A', 'A', 'B', 'C', 'D', 'E', 'F'],
+        'Column2': ['A', 'A', 'A', 'A', 'A', 'B', 'C', 'D', 'E', 'F'],
+        'Column3': ['A', 'A', 'A', 'A', 'A', 'B', 'C', 'D', 'E', 'F']
     }
     df = pd.DataFrame(data)
 
@@ -36,6 +43,11 @@ def test_rare_category_replacer_transform():
     transformed_df = replacer.fit_transform(df)
 
     # Check that the rare categories are replaced with the replacement value
-    assert transformed_df['Column1'].value_counts()['Others'] == 0
-    assert transformed_df['Column2'].value_counts()['Others'] == 0
-    assert transformed_df['Column3'].value_counts()['Others'] == 0
+    assert (transformed_df['Column1'] == 'Others').sum() == 5
+    assert (transformed_df['Column2'] == 'Others').sum() == 5
+    assert (transformed_df['Column3'] == 'Others').sum() == 5
+
+    # Check that the non-rare categories are not replaced
+    assert (transformed_df['Column1'] == 'A').sum() == 5
+    assert (transformed_df['Column2'] == 'A').sum() == 5
+    assert (transformed_df['Column3'] == 'A').sum() == 5
